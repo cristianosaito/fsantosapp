278e38c6ba1f8d1ff742ab515b26304cc796f4bf
cristianosaito <cristiano.saitodesouza@gmail.com>
2018-05-16 17:18:44 -0300

    comparacao cotacoes

--- src/pages/cotacao/cotacao.ts
+++ src/pages/cotacao/cotacao.ts
@@ -21,6 +21,11 @@ import { Observable } from 'rxjs/Observable';
 })
 export class CotacaoPage {
 
+  libraIcon: string;
+  dolarIcon: string;
+  yeneIcon: string;
+  euroIcon: string;
+
   dolar: any ;
   dolar_ontem:any;
   euro: any ;
@@ -30,10 +35,11 @@ export class CotacaoPage {
   libra: any ;
   libra_ontem:any;
 
-  data:Date =new Date();
-  data_show: string = this.data.getDay()+'/'+this.data.getMonth()+'/'+this.data.getFullYear();
+  moeda_1:any;
+  moeda_2:any;
 
-
+  data_show: any ;
+  
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams, 
@@ -44,6 +50,7 @@ export class CotacaoPage {
       this.getCotacao(21619);
       this.getCotacao(21621);
       this.getCotacao(21623);
+      this.data_show = this.dataFormatada();
   }
 
   async getCotacao(moeda:any){
@@ -52,7 +59,7 @@ export class CotacaoPage {
       data => {
         const response = (data as any);
         const obj_retorno = JSON.parse(response._body);
-        this.setMoedas(moeda,obj_retorno);
+        this.setMoedas(moeda, obj_retorno.valor_dia, obj_retorno.valor_dia_anterior);
         console.log(obj_retorno);
       
       }
@@ -60,38 +67,75 @@ export class CotacaoPage {
 
   }
 
-  setMoedas(moeda:any,dado:any){
+  setMoedas(moeda:any,dado:any, dado2:any){
 
     let valor = this.formataReal(parseFloat(dado));
+    let valor2 = this.formataReal(parseFloat(dado2));
+
+    let icon;
+
+    if (valor > valor2) {
+      icon = "arrow-round-up";
+    } else if (valor < valor2){
+      icon = "arrow-round-down";
+    } else if (valor == valor2){
+      icon = "";
+
+    }
+console.log(valor,valor2,icon);
 
     switch (moeda) {
       case 1:
         this.dolar = valor;
+        this.dolar_ontem = valor2;
+        this.dolarIcon = icon;
         break;
     
       case 21619:
         this.euro = valor;
+        this.euro_ontem = valor2;
+        this.euroIcon = icon;
         break;
 
       case 21623:
         this.libra = valor;
+        this.libra_ontem = valor2;
+        this.libraIcon = icon;
         break;
 
       case 21621:
         this.yene = valor;
+        this.yene_ontem = valor2;
+        this.yeneIcon = icon;
         break;
     }
   }
 
   formataReal(num) {
     num = (parseFloat(num)).toLocaleString('pt-BR', {
-      minimumFractionDigits: 2,
-      maximumFractionDigits: 2
+      minimumFractionDigits: 4,
+      maximumFractionDigits: 4
     });
     return num;
   }
 
+dataFormatada(){
+  var data = new Date();
 
+  var dia = data.getDate();
+  var dias = dia.toString();
+  if (dias.length == 1)
+    dias = "0" + dias;
+
+  var mes = data.getMonth() + 1;
+  var mess = mes.toString();
+  if (mess.length == 1)
+    mess = "0" + mess;
+
+  var ano = data.getFullYear();
+
+  return dias + "/" + mess + "/" + ano;
+}
   ionViewDidLoad() {
   
    
